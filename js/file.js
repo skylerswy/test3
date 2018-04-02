fa4dee23f9a2182af496d6e3bd9cb2ae4d68a06b
skylerswy <56240597@qq.com>
2018-04-02 11:13:55 +0800

    js搜索框

--- js/file.js
+++ js/file.js
@@ -1,16 +1,20 @@
 var sear = document.getElementById("sear");
-var dis = document.getElementsByClassName("input-bar");
+var find = document.getElementById("sousuo");
+//console.log(find);
+//var dis = document.getElementsByClassName("input-bar");
 //console.log(dis[0]);
 sear.addEventListener("click",sear_for);
 function sear_for() {
    //console.log(sear);
-   var st = getComputedStyle(dis[0], null);
+   var st = getComputedStyle(find, null);
    console.log(st);
    var tye = st.getPropertyValue("display");
    //console.log(tye);
    if (tye == "none"){
+        find.style.display='block';
         //st.getPropertyValue("display")="block";
    }else{
+        find.style.display='none';
         //st.getPropertyValue("display")="none";
    }
 }
